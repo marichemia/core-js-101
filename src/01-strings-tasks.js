2c0266730d8e14756decf9b190f2fcde2b65b719
marichemia <marichemia10@gmail.com>
2022-07-19 20:35:27 +0400

    solved all string tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -280,7 +280,7 @@ function isString(value) {
  */
 function getCardId(value) {
   const suits = ['♣', '♦', '♥', '♠'];
-  const values = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
   let number = 0;
 
