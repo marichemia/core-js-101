c5ebe63a4ccd5bf873828bf777a2a799df5579b8
marichemia <marichemia10@gmail.com>
2022-07-19 21:56:17 +0400

    daf

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -125,7 +125,7 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
+function getLastDigit(value) {
   return +value.toString()[value.toString().length - 1];
 }
 
