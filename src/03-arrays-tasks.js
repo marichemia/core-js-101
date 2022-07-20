a5bf23e23837c75390b7f01366a13bffdd5d05c0
marichemia <marichemia10@gmail.com>
2022-07-20 19:00:39 +0400

    get3TopItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -295,8 +295,9 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  arr.sort((a, b) => a - b);
+  return arr.splice(arr.length - 3);
 }
 
 
