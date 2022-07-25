d5fe7b2e109e70cb9f70d49c437ec3a9d44fcb2c
marichemia <marichemia10@gmail.com>
2022-07-25 16:55:40 +0400

    retry

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -113,8 +113,16 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  try {
+    func();
+  } catch (error) {
+    if (attempts >= 1) {
+      retry(func, attempts - 1);
+    }
+  }
+
+  return func();
 }
 
 
