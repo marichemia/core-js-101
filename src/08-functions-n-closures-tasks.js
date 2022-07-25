330d3c9e0893672bfacc3f9837ba01f91a1af383
marichemia <marichemia10@gmail.com>
2022-07-25 17:54:17 +0400

    g

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -189,8 +189,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let num = startFrom;
+  return () => {
+    num += 1;
+    return num;
+  };
 }
 
 
